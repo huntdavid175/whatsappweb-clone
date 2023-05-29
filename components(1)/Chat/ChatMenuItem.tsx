134860438fc4829ab9eac3da8f3d2dba91366626
huntdavid175 <huntdavid175@gmail.com>
2023-05-29 08:39:29 +0000

    folder name

---
+++ components(1)/Chat/ChatMenuItem.tsx
@@ -0,0 +1,73 @@
+import React from "react";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import Divider from "@mui/material/Divider";
+import ListItemText from "@mui/material/ListItemText";
+import ListItemAvatar from "@mui/material/ListItemAvatar";
+import Avatar from "@mui/material/Avatar";
+import Typography from "@mui/material/Typography";
+import DoneAllIcon from "@mui/icons-material/DoneAll";
+import { ListItemButton } from "@mui/material";
+import { IoIosDoneAll } from "react-icons/io";
+
+const ChatMenuItem = ({ read }: { read: boolean }) => {
+  return (
+    <>
+      {/* <ListItemButton> */}
+      <ListItem alignItems="flex-start" button>
+        <ListItemAvatar>
+          <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+        </ListItemAvatar>
+        {/* <ListItemText
+            sx={{
+              width: "10px",
+            }}
+            primary="Ali Connors"
+            secondary={
+              <React.Fragment>
+                <Typography
+                  sx={{
+                    display: "inline",
+                    overflow: "hidden",
+                  }}
+                  component="span"
+                  variant="body2"
+                  color="text.primary"
+                  noWrap={true}
+                >
+                  I'll be in your neighborhood doing errands this and life will
+                  treat me good and for God so loved the world that he
+                </Typography>
+
+               
+              </React.Fragment>
+            }
+          /> */}
+        <div className="w-full m-auto h-full ">
+          <div className="flex w-full justify-between">
+            <p>Ali Pio</p>
+            <span className="text-xs text-iconsdeep">4/14/2023</span>
+          </div>
+          <div className="text-sm w-[290px] min-w-[300px] ">
+            <Typography noWrap sx={{ fontSize: "0.875rem", color: "#54656F" }}>
+              <span
+                className={`inline-block ${
+                  read ? " text-blue-400" : "text-iconsdeep"
+                } mr-1`}
+              >
+                <DoneAllIcon sx={{ fontSize: "1.2rem" }} />
+                {/* <IoIosDoneAll /> */}
+              </span>
+              Long message can go here i promise you and i will make it worth
+              your while
+            </Typography>
+          </div>
+        </div>
+      </ListItem>
+      {/* </ListItemButton> */}
+      <Divider variant="inset" component="li" />
+    </>
+  );
+};
+
+export default ChatMenuItem;
---
+++ components(1)/Chat/Chatbox.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import React from "react";
+
+import ChatSectionHeader from "./ChatSectionHeader";
+import ChatFooter from "./ChatFooter";
+import ChatDrawer from "../UI/ChatDrawer";
+import SpeedDIal from "../UI/SpeedDial";
+import MessageBubble from "./MessageBubble";
+import ChatDate from "../UI/ChatDate";
+
+const Chatbox = () => {
+  return (
+    <>
+      <div className="h-full flex-shrink-0 flex flex-col grow min-w-[700px] ">
+        <ChatSectionHeader />
+        <div className="w-full   flex-1  overflow-scroll bg-whatsapp-light scrollbar pt-8 px-8 py-1">
+          <ChatDate date=" 5/13/2023" />
+          <MessageBubble
+            self={false}
+            forwarded
+            message=" MessageBubble this is where messages re written i honestly don't know
+        why its like that but i hpe it ends soon"
+          />
+
+          <MessageBubble self={false} forwarded={false} message="*" />
+
+          <MessageBubble
+            self={true}
+            forwarded={false}
+            message="I don't get you "
+          />
+          <MessageBubble self={true} forwarded={false} message=". " />
+          <MessageBubble
+            self={true}
+            forwarded={false}
+            message="Honestly i don't see the reason why you should be angry "
+          />
+          <MessageBubble self={false} forwarded={false} message="Lol " />
+          <MessageBubble
+            self={false}
+            forwarded={false}
+            message="You are joking right ? "
+          />
+          <ChatDate date="TODAY" />
+          <MessageBubble self={true} forwarded={false} message="Lol " />
+          <MessageBubble self={false} forwarded={true} message="I hate you " />
+          <MessageBubble
+            self={false}
+            forwarded={false}
+            message="That is what she sent to me "
+          />
+
+          <MessageBubble self={false} forwarded={true} message="I hate you " />
+          <MessageBubble self={true} forwarded={false} message="Goats " />
+        </div>
+
+        <ChatFooter />
+      </div>
+    </>
+  );
+};
+
+export default Chatbox;
